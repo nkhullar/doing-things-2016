2d3b19347c739dff6283231b7b3e21cba0cfcf14
Namit Khullar <namit_khullar@hotmail.co.uk>
2016-07-09 16:06:04 -0400

    more stuff to add

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 
 public class Calculator{
 	public static void main(String[] args){
-	
+		String op;
+		double num1, num2;
+		Scanner inputOp = new Scanner(System.in);
+		Scanner inputNum = new Scanner(System.in);
+
+		System.out.print("Enter the first number: ");
+		num1 = inputNum.nextInt();
+		System.out.print("Enter the second number: ");
+		num2 = inputNum.nextInt();
+		System.out.print("Enter the operator");
+		op = inputOp.next();
+
+		if(op.equals("+")){
+			System.out.println("Sum is: " + (num1 + num2));
+		} else if(op.equals("-")){
+			System.out.println("Difference is: " + (num2 - num1));
+		} else if (op.equals("*")){
+			System.out.println("Multiplication of two number is: " + (num1*num2));
+		} else if (op.equals("/")){
+			System.out.println("Division of two numbers is :" + (num1/num2));
+		}
 	}
 }
--- Relational.java
+++ Relational.java
@@ -2,12 +2,7 @@ import java.util.*;
 
 public class Relational{
 	public static void main(String[] args){
-
-		int hoursWorked = 43;
-		int salary = 47000;
-
-		System.out.println((salary < 50000) && (hoursWorked >40));
 		System.out.println("Hello" == "Hello");
-		System.out.println(2 >= 2);
+		System.out.println(2 == 2);
 	}
 }
